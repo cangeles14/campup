c984f0fb06dfc7c178b73b795c4b0b008788237c
Christopher Angeles <christopher.angeles@gmail.com>
2018-01-30 10:30:34 +0000

    add mlab

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentRoutes    = require ("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://christopher:chris14@ds119078.mlab.com:19078/campup");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
